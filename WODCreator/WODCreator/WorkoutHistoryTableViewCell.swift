8b976e8ffe7e872bbf1f314edc267aedf7c73185
Lili Shi <lili@kayak.com>
2018-02-19 19:32:24 -0500

    first

---
+++ WODCreator/WODCreator/WorkoutHistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutHistoryTableViewCell.swift
+//  WODCreator
+//
+//  Created by Lili Shi on 2/19/18.
+//  Copyright © 2018 Lili Shi. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
